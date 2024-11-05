d54fcca6992e875e9f5db7ee85a73d81bd35ade9
artem-mutaf <123882882+artem-mutaf@users.noreply.github.com>
2024-11-06 00:34:46 +0200

    Первый коммит

---
+++ DrugsBot/Domain/ValueObjects/Address.cs
@@ -0,0 +1,33 @@
+namespace DrugsBot.Domain.ValueObjects;
+
+public class Address : BaseValueObject
+{
+    public Address(string city, string street, string house)
+    {
+        City = city;
+        Street = street;
+        House = house;
+    }
+    
+    /// <summary>
+    /// Город аптеки.
+    /// </summary>
+    public string City { get; private set; }
+    /// <summary>
+    /// Улица аптеки.
+    /// </summary>
+    public string Street { get; private set; }
+    /// <summary>
+    /// № Дома.
+    /// </summary>
+    public string House { get; private set; }
+
+    /// <summary>
+    /// Возвращает строковое представление адреса
+    /// </summary>
+    /// <returns>Строка, представляющая адрес</returns>
+    public override string ToString()
+    {
+        return $"{City}, {Street}, {House}";
+    }
+}
